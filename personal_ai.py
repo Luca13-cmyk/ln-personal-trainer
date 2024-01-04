607a03535c26d4036797328602f4ec9e39c76f36
Luca <lucanegresco@gmail.com>
2024-01-04 11:02:42 -0300

    resource path

--- personal_ai.py
+++ personal_ai.py
@@ -25,11 +25,10 @@ def resource_path(relative_path):
 
 
 model_path=resource_path("pose_landmarker_full.task")
-
-
+# The Perfect Push Up _ Do it right! - YouTube e mais 13 páginas - Pessoal — Microsoft​ Edge 2023-12-29 02-38-27.mp4
 class PersonalAI:
-    def __init__(self, file_name="The Perfect Push Up _ Do it right! - YouTube e mais 13 páginas - Pessoal — Microsoft​ Edge 2023-12-29 02-38-27.mp4"):
-        self.file_name = resource_path(file_name)
+    def __init__(self, file_name="https://utfs.io/f/d063562d-9c7f-4e90-9dc9-c1d5055a4255-ai6rjk.mp4"):
+        self.file_name = file_name
         self.image_q = queue.Queue()
 
         self.options = python.vision.PoseLandmarkerOptions(
